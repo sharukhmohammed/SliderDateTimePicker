d8f0dc8a54dced8561ece005dbb90aaf32b2cbdb
Sharukh Mohammed <sharukhmohammed@me.com>
2018-11-02 18:49:17 +0530

    Initial Commit

---
+++ wsdtpicker/src/main/java/sharukh/wsdtpicker/TimeAdapter.java
@@ -0,0 +1,121 @@
+package sharukh.wsdtpicker;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+class TimeAdapter extends RecyclerView.Adapter<TimeAdapter.Holder> {
+
+    private final ArrayList<String> times = new ArrayList<String>() {{
+        add("12 AM");
+        add("1 AM");
+        add("2 AM");
+        add("3 AM");
+        add("4 AM");
+        add("5 AM");
+        add("6 AM");
+        add("7 AM");
+        add("8 AM");
+        add("9 AM");
+        add("10 AM");
+        add("11 AM");
+        add("12 AM");
+        add("1 PM");
+        add("2 PM");
+        add("3 PM");
+        add("4 PM");
+        add("5 PM");
+        add("6 PM");
+        add("7 PM");
+        add("8 PM");
+        add("9 PM");
+        add("10 PM");
+        add("11 PM");
+    }};
+
+    private int selectedPos;
+    private int disableBefore = -1;
+    private OnTimeSelectedListener onTimeSelected;
+    private Context context;
+
+    TimeAdapter(int selectedPos, OnTimeSelectedListener onTimeSelectedListener) {
+
+        if (onTimeSelectedListener != null)
+            this.onTimeSelected = onTimeSelectedListener;
+        else
+            throw new IllegalStateException("Not implemented");
+
+        this.selectedPos = selectedPos;
+    }
+
+    public void setDisableBefore(int disableBeforeHrs) {
+        this.disableBefore = disableBeforeHrs;
+        notifyDataSetChanged();
+    }
+
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        context = viewGroup.getContext();
+        return new Holder(
+                LayoutInflater.from(viewGroup.getContext())
+                        .inflate(R.layout.item_time, viewGroup, false)
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final Holder holder, int pos) {
+        holder.timeText.setText(times.get(pos));
+
+        if (pos > disableBefore) {
+            if (pos == selectedPos) {
+                holder.itemView.setBackground(ContextCompat.getDrawable(context, R.drawable.gradient_primary));
+                holder.timeText.setTextColor(ContextCompat.getColor(context, R.color.white));
+            } else {
+                holder.itemView.setBackgroundColor(ContextCompat.getColor(context, R.color.white));
+                holder.timeText.setTextColor(ContextCompat.getColor(context, R.color.text_dark));
+            }
+
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    selectedPos = holder.getAdapterPosition();
+                    onTimeSelected.onTimeSelected(holder.getAdapterPosition());
+                    notifyDataSetChanged();
+                }
+            });
+        } else {
+            holder.timeText.setTextColor(ContextCompat.getColor(context, R.color.text_medium));
+            holder.itemView.setBackground(ContextCompat.getDrawable(context, R.color.white));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return times.size();
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+        TextView timeText;
+
+        Holder(@NonNull View itemView) {
+            super(itemView);
+            timeText = itemView.findViewById(R.id.item_time_text);
+        }
+    }
+
+    interface OnTimeSelectedListener {
+        void onTimeSelected(int hourOfTheDay);
+    }
+
+
+}
---
+++ wsdtpicker/src/main/java/sharukh/wsdtpicker/WSDateTimePicker.java
@@ -0,0 +1,152 @@
+package sharukh.wsdtpicker;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSnapHelper;
+import android.support.v7.widget.PagerSnapHelper;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+import devs.mulham.horizontalcalendar.HorizontalCalendar;
+import devs.mulham.horizontalcalendar.utils.HorizontalCalendarListener;
+
+public class WSDateTimePicker extends BottomSheetDialogFragment {
+
+    /* Private Fields
+     * */
+    private static final String TAG = "BottomSlidingDateTime";
+    private static final String INPUT_TITLE = "Title";
+    private Calendar selectedCal;
+    private OnDateTimeSetListener dateTimeSetListener;
+
+    /* Public Usage Fields
+     * */
+    public static String fragTitle = "";
+
+    public static WSDateTimePicker newInstance(String title) {
+
+        Bundle args = new Bundle();
+        args.putString(INPUT_TITLE, title);
+        WSDateTimePicker fragment = new WSDateTimePicker();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public void setOnDateTimeSetListener(OnDateTimeSetListener onDateTimeSetListener) {
+        if (onDateTimeSetListener != null) {
+            this.dateTimeSetListener = onDateTimeSetListener;
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_date_time_picker, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (getArguments() != null) {
+            fragTitle = getArguments().getString(INPUT_TITLE);
+        }
+
+        final TextView selected_date_time = view.findViewById(R.id.frag_title);
+        selected_date_time.setText(fragTitle);
+
+        selectedCal = Calendar.getInstance();
+
+        /* starts after 1 hour from now */
+        Calendar startDate = Calendar.getInstance();
+        startDate.add(Calendar.HOUR, 1);
+        startDate.set(Calendar.MINUTE, 0);
+        startDate.set(Calendar.SECOND, 0);
+
+        /* ends after 2 years from now */
+        Calendar endDate = Calendar.getInstance();
+        endDate.add(Calendar.YEAR, 2);
+
+        HorizontalCalendar date_picker = new HorizontalCalendar.Builder(view, R.id.frag_date_picker)
+                .range(startDate, endDate)
+                .datesNumberOnScreen(5)
+                .build();
+
+
+        final TimeAdapter adapter = new TimeAdapter(selectedCal.get(Calendar.HOUR_OF_DAY), new TimeAdapter.OnTimeSelectedListener() {
+            @Override
+            public void onTimeSelected(int hourOfTheDay) {
+                selectedCal.set(Calendar.HOUR_OF_DAY, hourOfTheDay);
+            }
+        });
+
+        final RecyclerView time_recycler = view.findViewById(R.id.frag_time_picker);
+        time_recycler.setAdapter(adapter);
+
+        LinearSnapHelper snapHelper = new LinearSnapHelper();
+        snapHelper.attachToRecyclerView(time_recycler);
+
+        date_picker.setCalendarListener(new HorizontalCalendarListener() {
+            @Override
+            public void onDateSelected(Calendar date, int position) {
+                selectedCal = (Calendar) date.clone();
+
+                int today = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+
+                if (selectedCal.get(Calendar.DAY_OF_MONTH) == today) {
+                    adapter.setDisableBefore(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+                } else {
+                    adapter.setDisableBefore(-1);
+                }
+
+                int hourOfDay = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+                time_recycler.smoothScrollToPosition(hourOfDay);
+
+            }
+        });
+
+        //Calling this for first time
+        date_picker.selectDate(startDate, true);
+
+
+        time_recycler.smoothScrollToPosition(Calendar.getInstance().get(Calendar.HOUR_OF_DAY));
+
+
+        Button done_button = view.findViewById(R.id.frag_done);
+        done_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dateTimeSetListener.onDateTimeSelected(selectedCal);
+                dismissAllowingStateLoss();
+            }
+        });
+
+
+    }
+
+
+    private void initVars() {
+
+    }
+
+    private void initViews() {
+
+    }
+
+
+    public interface OnDateTimeSetListener {
+        void onDateTimeSelected(Calendar selectedDateTime);
+    }
+
+
+}
